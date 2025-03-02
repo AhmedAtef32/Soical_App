0f99a55dcbb14a986bf44e2cf78159e7d2d86e2c
Ahmed Atef <Ahmedatef32003@gmail.com>
2025-03-02 15:06:07 +0200

    upload project

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './shared/services/flowbite/flowbite.service';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  title = 'social_App';
+
+  private flowbiteService = inject(FlowbiteService)
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
+}
---
+++ src/app/feature/pages/post-detailes/post-detailes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-detailes',
+  imports: [],
+  templateUrl: './post-detailes.component.html',
+  styleUrl: './post-detailes.component.scss'
+})
+export class PostDetailesComponent {
+
+}
